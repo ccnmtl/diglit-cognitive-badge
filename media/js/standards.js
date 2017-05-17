81af5472ab1c65fc7eb273428a01d5bf2cb22f48
Susan Dreher <sdreher@columbia.edu>
2017-05-17 12:04:50 -0400

    reworking script to allow arbitrary categories

--- media/js/standards.js
+++ media/js/standards.js
@@ -1,5 +1,28 @@
-/* global Backbone: true, _: true, jQuery: true */
+/* global Backbone: true, _: true, jQuery: true, Papa: true */
 
+var Lens = Backbone.Model.extend({
+    defaults: {
+        name: '',
+        description: ''
+    },
+    toTemplate: function() {
+        return _(this.attributes).clone();
+    }
+});
+
+var LensList = Backbone.Collection.extend({
+    model: Lens,
+    toTemplate: function() {
+        var a = [];
+        this.forEach(function(item) {
+            a.push(item.toTemplate());
+        });
+        return a;
+    },
+    byName: function(name) {
+        return this.findWhere({name: name});
+    },
+});
 
 var Standard = Backbone.Model.extend({
     defaults: {
@@ -8,7 +31,6 @@ var Standard = Backbone.Model.extend({
         return _(this.attributes).clone();
     }
 });
-
 
 var StandardList = Backbone.Collection.extend({
     model: Standard,
@@ -29,77 +51,136 @@ var StandardList = Backbone.Collection.extend({
     },
     byId: function(id) {
         return this.findWhere({id: id});
-    },
-    byType: function(type) {
-        var lst = this.where({Type: type});
-        for (var i=0; i < lst.length; i++) {
-            lst[i] = lst[i].toTemplate();
-        }
-        return lst;
     }
 });
 
+var State = Backbone.Model.extend({
+    FIELD_ACTIVE: 'Active (y/n)',
+    FIELD_LENS_NAME: 'Type',
+    FIELD_LENS_DESCRIPTION: 'Type Description',
+    defaults: {
+        activeStandard: undefined,
+        activeLens: undefined
+    },
+    initialize: function(description) {
+        var allLens = new Lens({
+            name: 'All Competencies',
+            href: 'all_competencies',
+            description: description
+        });
+
+        var lenses = new LensList([allLens]);
+        this.set('lenses', lenses);
+
+        this.set('standards', new StandardList());
+    },
+    initStandards: function(results) {
+        for (var i = 0; i < results.data.length; i++) {
+            if (!this.validStandard(results.data[i])) {
+                continue;
+            }
+
+            var std = new Standard(results.data[i]);
+            std.set('id', i);
+            this.get('standards').add(std);
+
+            var lensName = results.data[i][this.FIELD_LENS_NAME];
+            if (!this.get('lenses').byName(lensName)) {
+                var lens = new Lens({
+                    name: lensName,
+                    filter: lensName,
+                    href: this.slugify(lensName),
+                    description: results.data[i][this.FIELD_LENS_DESCRIPTION]
+                });
+                this.get('lenses').add(lens);
+            }
+        }
+        this.set('activeLens', this.get('lenses').at(0));
+    },
+    context: function() {
+        var ctx = {
+            'standards': this.get('standards').toTemplate(),
+            'lenses': this.get('lenses').toTemplate(),
+            'activeLens': this.get('activeLens').toTemplate()
+        };
+
+        var std = this.get('activeStandard');
+        if (std) {
+            ctx.activeStandard = std.toTemplate();
+        }
+        return ctx;
+    },
+    slugify: function(str) {
+        return str.toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'-');
+    },
+    validStandard: function(row) {
+        var value = row[this.FIELD_ACTIVE];
+        return value === 'y' || value === 'Y';
+    }
+});
 
 var StandardView = Backbone.View.extend({
     events: {
         'click a[href="#"]': 'passiveClick',
-        'click .competency-detail': 'onDetail'
+        'click a.nav-link': 'onLens',
+        'click .competency-detail': 'onShowDetail'
     },
     initialize: function(options) {
-        _.bindAll(this, 'render', 'initStandards', 'passiveClick', 'onDetail');
+        _.bindAll(this, 'render', 'onLens', 'onShowDetail', 'onHideDetail',
+            'addHistory', 'popHistory');
 
-        this.competencies = ['technical', 'administrative', 'intellectual'];
+        this.state = new State(options.allDescription);
+        this.state.on('change:activeStandard', this.render);
+        this.state.on('change:activeLens', this.render);
 
-        this.allTemplate = _.template(options.allTemplate.html());
-        this.coreTemplate = _.template(options.coreTemplate.html());
+        jQuery('#competency-detail-modal').on(
+            'hidden.bs.modal', this.onHideDetail);
+
+        this.lensTemplate = _.template(options.lensTemplate.html());
+        this.standardsTemplate = _.template(options.standardsTemplate.html());
         this.detailTemplate = _.template(options.detailTemplate.html());
 
+        var self = this;
         Papa.parse(options.url, {
             download: true,
             header: true,
-            complete: this.initStandards
+            complete: function(results) {
+                self.state.initStandards(results);
+            }
         });
     },
-    passiveClick: function(evt) {
-        evt.preventDefault();
-        return false;
+    addHistory: function(lens, standard) {
     },
-    activeCompetency: function(row) {
-        var FIELD_ACTIVE = 'Active (y/n)';
-        var value = row[FIELD_ACTIVE];
-        return value === 'y' || value === 'Y';
-    },
-    initStandards: function(results) {
-        this.standards = new StandardList();
-        for (var i=0; i < results.data.length; i++) {
-            if (this.activeCompetency(results.data[i])) {
-                var std = new Standard(results.data[i]);
-                std.set('id', i);
-                this.standards.add(std);
-            }
-        }
-        this.render();
+    popHistory: function() {
     },
     render: function() {
-        var ctx = {'standards': this.standards.toTemplate()};
-        var $elt = this.$el.find('.standards-container.all');
-        $elt.html(this.allTemplate(ctx));
+        var ctx = this.state.context();
 
-        for (var i=0; i < this.competencies.length; i++) {
-            var type = this.competencies[i];
-            ctx = {'standards': this.standards.byType(type)};
-            $elt = this.$el.find('.standards-container.' + type);
-            $elt.html(this.coreTemplate(ctx));
+        var $elt = this.$el.find('.standards-container');
+        $elt.html(this.standardsTemplate(ctx));
+
+        $elt = this.$el.find('.lens-container');
+        $elt.html(this.lensTemplate(ctx));
+
+        if (ctx.activeStandard) {
+            var $modal = jQuery('#competency-detail-modal');
+            $modal.find('.modal-body').html(this.detailTemplate(ctx));
+            $modal.modal('show');
         }
     },
-    onDetail: function(evt) {
-        var $modal = jQuery('#competency-detail-modal');
-
-        // update modal based on the competency id attributes
-        var std = this.standards.byId(jQuery(evt.currentTarget).data('id'));
-        var ctx = {'standard': std.toTemplate()};
-
-        $modal.find('.modal-body').html(this.detailTemplate(ctx));
-        $modal.modal('show');
+    onLens: function(evt) {
+        evt.preventDefault();
+        var lensName = jQuery(evt.currentTarget).data('name');
+        var lens = this.state.get('lenses').byName(lensName);
+        this.state.set('activeLens', lens);
+    },
+    onShowDetail: function(evt) {
+        evt.preventDefault();
+        var id = jQuery(evt.currentTarget).data('id');
+        var std = this.state.get('standards').byId(id);
+        this.state.set('activeStandard', std);
+    },
+    onHideDetail: function(evt) {
+        this.state.set('activeStandard', undefined);
     }
 });
